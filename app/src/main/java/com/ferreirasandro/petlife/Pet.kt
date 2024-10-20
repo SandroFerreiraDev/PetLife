bccb41dddcb3802b58fa1a5b26bdf1d8f1e88b06
SandroFerreiraDev <sandro.ferreira.developer@gmail.com>
2024-10-20 15:37:06 -0300

    feat: Added Pet class

---
+++ app/src/main/java/com/ferreirasandro/petlife/Pet.kt
@@ -0,0 +1,11 @@
+package com.ferreirasandro.petlife
+data class Pet(
+    var name: String,
+    var birthDate: String,
+    var type: String,
+    var color: String,
+    var size: String,
+    var lastVetVisit: String,
+    var lastVaccination: String,
+    var lastPetShopVisit: String
+)
