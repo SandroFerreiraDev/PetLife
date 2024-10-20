8ab32de3690ba94f14035a25267c10f98a1f61bb
SandroFerreiraDev <sandro.ferreira.developer@gmail.com>
2024-10-20 16:00:04 -0300

    feat: added MainActivity screen

--- app/src/main/java/com/ferreirasandro/petlife/MainActivity.kt
+++ app/src/main/java/com/ferreirasandro/petlife/MainActivity.kt
@@ -1,20 +1,53 @@
 package com.ferreirasandro.petlife
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ferreirasandro.petlife.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var pet: Pet
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        pet = Pet(
+            name = "Rex",
+            birthDate = "01/01/2020",
+            type = "Cão",
+            color = "Marrom",
+            size = "Médio",
+            lastVetVisit = "01/09/2024",
+            lastVaccination = "15/08/2024",
+            lastPetShopVisit = "10/08/2024"
+        )
+
+        updateUI()
+
+        binding.btnEditPet.setOnClickListener {
+            val intent = Intent(this, EditPetActivity::class.java)
+            intent.putExtra("pet", pet)
+            startActivityForResult(intent, 1)
+        }
+    }
+
+    private fun updateUI() {
+        binding.tvPetName.text = "Nome: ${pet.name}"
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            pet = data?.getSerializableExtra("pet") as Pet
+            updateUI()
         }
     }
 }
